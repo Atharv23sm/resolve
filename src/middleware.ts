1fbf09d5cf1e059bfb9c094a71442d9d31ea68e8
Atharv Mahabal <122306441+Atharv23sm@users.noreply.github.com>
2024-07-08 18:56:19 +0530

    Update middleware.ts
    Redirecting authorised users to home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,12 @@ export function middleware(req: NextRequest) {
 
   const token = req.cookies.get("token")?.value || "";
 
+  if ((path==="/login" || path==="/signup") && token) {
+    return NextResponse.redirect(new URL("/home", req.nextUrl));
+  }
   if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
-}
 
 export const config = {
   matcher: [
